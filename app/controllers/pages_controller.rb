1eb2f21b0691762756b3d30ab527c339d5ec25f4
Onur Uyar <me@onuruyar.com>
2015-08-05 14:38:32 +0100

    allow iframe

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+
+  before_action :allow_iframe
+
   def index
+  end
+  
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
